301b37b0b5ce2ae42b7f6e642b1bee0d40f319b0
Henk Kalkwater <cjljosten@gmail.com>
2020-03-19 14:34:23 +0100

    Added decrypted data

--- misc/wiifitmeter.lua
+++ misc/wiifitmeter.lua
@@ -1,4 +1,5 @@
 wfmtrnet_protocol = Proto("FitMtrNet", "Wii Fit Meter Network Layer protocol") 
+wfmtrtrn_protocol = Proto("FitMtrTrn", "Wii Fit Meter Transport Layer Protocol")
 -- CRC-8
 crc_table = {}
 di = 0x07
@@ -33,6 +34,31 @@ function crc8_list(list)
 	end
 	return crc
 end
+
+function decrypt_data(list)
+	size = list:len()
+	i = 0
+	for i = 0, size - 1, 1
+	do
+		list:set_index(i, bit32.bxor(list:get_index(i), 0x55))
+	end
+	return list
+	--[[while size >= 8 do
+		for j = 8, 16, 1 
+		do
+			list:set_index(i + j, bit32.bxor(list:get_index(i + j), 0x55))
+		end
+		i = i + 8
+		size = size - 8
+	end
+	while size > 0 do
+		list:set_index(i, bit32.bxor(list:get_index(i), 0x55))
+		i = i + 1
+		size = size - 1
+	end
+	return list]]
+end
+
 --message("test")
 --HEADERS
 header_magic = ProtoField.uint8("fitmtrnet.magic", "Magic Number", base.HEX)
@@ -41,8 +67,9 @@ header_connection_setup = ProtoField.bool("fitmtrnet.connection_setup", "Connect
 header_data_length = ProtoField.int8("fitmtrnet.length", "Data Length")
 header_checksum = ProtoField.uint8("fitmtrnet.crc", "CRC-8")
 header_data = ProtoField.bytes("fitmtrnet.data", "Data")
+header_decrypted_data = ProtoField.bytes("fitmtrnet.decrypted_data", "Decrypted data")
 	
-wfmtrnet_protocol.fields = {header_magic, header_connection, header_connection_setup, header_data_length, header_checksum, header_data}
+wfmtrnet_protocol.fields = {header_magic, header_connection, header_connection_setup, header_data_length, header_checksum, header_data, header_decrypted_data}
 
 function wfmtrnet_protocol.dissector(buffer, pinfo, tree)
 	length = buffer:len()
@@ -56,17 +83,27 @@ function wfmtrnet_protocol.dissector(buffer, pinfo, tree)
 	
 	connection_setup_flag = bit32.band(buffer(2, 1):le_uint(), 0x80) == 0x80
 	subtree:add_le(header_connection_setup, buffer(2,1), connection_setup_flag)
+	if connection_setup_flag then
+		pinfo.cols.info = "Connection Setup"
+	end
 	
 	-- If the length (3th byte), has the flag 0x40 set, the actual length is contained in the next byte
 	if bit32.band(buffer(2, 1):le_uint(), 0x40) == 0x40 then
-		subtree:add_le(header_data_length, buffer(3, 1))
+		data_length = buffer(3,1):le_uint()
+		subtree:add_le(header_data_length, data_length)
 		subtree:add_le(header_data, buffer(4, buffer(3, 1):le_uint()), "Wii Fit Meter Data")
+		if not connection_setup_flag then
+			subtree:add_le(header_decrypted_data, buffer(4, data_length), decrypt_data(buffer(4, data_length)))
+		end
 	else
 		-- 0x3F = 0b00111111
 		data_length = bit32.band(buffer(2,1):le_uint(), 0x3F)
 		--report_failure(length)
 		subtree:add_le(header_data_length, buffer(2, 1), data_length)
 		subtree:add_le(header_data, buffer(3, data_length))
+		if not connection_setup_flag then
+			subtree:add_le(header_decrypted_data, buffer(3, data_length), decrypt_data(buffer(3, data_length):bytes()):raw())
+		end
 	end
 	subtree:add_le(header_checksum, buffer(length - 1, 1)):append_text(" (Expected: "..crc8_list(buffer(0, length - 1):bytes())..")")
 	--crc8_list(buffer(0, length - 1):bytes())
